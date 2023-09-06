7f4737ecf90f1383498f9d48afce99188c44613f
Dominik Hasiwar <dominik.hasiwar@gmail.com>
2023-09-06 23:02:23 +0200

    Optimize db initialization

--- src/Mcce.SmartOffice.Bookings/Bootstrap.cs
+++ src/Mcce.SmartOffice.Bookings/Bootstrap.cs
@@ -2,7 +2,7 @@
 using Mcce.SmartOffice.Bookings.Managers;
 using Mcce.SmartOffice.Bookings.Services;
 using Mcce.SmartOffice.Core;
-using Mcce.SmartOffice.Core.Enums;
+using Mcce.SmartOffice.Core.Extensions;
 using Microsoft.EntityFrameworkCore;
 
 namespace Mcce.SmartOffice.Bookings
@@ -11,28 +11,16 @@ namespace Mcce.SmartOffice.Bookings
     {
         protected override void OnConfigureBuilder(WebApplicationBuilder builder)
         {
-            switch (AppConfig.DbConfig.DbType)
-            {
-                case DbType.SQLite:
-                    builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlite(AppConfig.DbConfig.ConnectionString));
-                    break;
-                default:
-                    throw new InvalidOperationException($"The database type '{AppConfig.DbConfig.DbType}' is not supported!");
-            }
+            builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlite(AppConfig.DbConfig.ConnectionString));
 
             builder.Services.AddScoped<IBookingManager, BookingManager>();
 
             builder.Services.AddScoped<IEmailService>(s => new EmailService(AppConfig.EmailConfig));
-
         }
 
         protected override async Task OnConfigureApp(WebApplication app)
         {
-            using var scope = app.Services.CreateScope();
-
-            var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-
-            await db.Database.MigrateAsync();
+            await app.Services.InitializeDatabase<AppDbContext>();
         }
     }
 }
---
+++ src/Mcce.SmartOffice.Core/Extensions/ServiceProviderExtension.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+
+namespace Mcce.SmartOffice.Core.Extensions
+{
+    public static class ServiceProviderExtension
+    {
+        public static async Task InitializeDatabase<T>(this IServiceProvider serviceProvider) where T : DbContext
+        {
+            using var scope = serviceProvider.CreateScope();
+
+            var dbContext = scope.ServiceProvider.GetRequiredService<T>();
+
+            if (dbContext.Database.IsSqlite())
+            {
+                var connectionString = dbContext.Database.GetConnectionString();
+
+                var sb = new SqliteConnectionStringBuilder(connectionString);
+
+                var directory = Path.GetDirectoryName(sb.DataSource);
+
+                if (!Directory.Exists(directory))
+                {
+                    Log.Information($"Creating directory '{directory}' for database.");
+                    Directory.CreateDirectory(directory);
+                }
+
+                await dbContext.Database.MigrateAsync();
+            }
+        }
+    }
+}
