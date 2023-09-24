aab622363783f9451bce7c19ecd6ccb529c75e53
Dominik Hasiwar <dominik.hasiwar@gmail.com>
2023-09-24 21:42:28 +0200

    Add k8s files for local deployment

--- src/Mcce.SmartOffice.Core/Configs/AuthConfig.cs
+++ src/Mcce.SmartOffice.Core/Configs/AuthConfig.cs
@@ -4,6 +4,13 @@
     {
         public string AuthUrl { get; set; }
 
+        private string _authFrontendUrl;
+        public string AuthFrontendUrl
+        {
+            get { return _authFrontendUrl ?? AuthUrl; }
+            set { _authFrontendUrl = value; }
+        }
+
         public string ClientId { get; set; }
     }
 }
