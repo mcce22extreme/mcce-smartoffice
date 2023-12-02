2680a02c02a3d36d8825d40ee7e985340e78e3d0
Dominik Hasiwar <dominik.hasiwar@gmail.com>
2023-12-02 09:43:39 +0100

    Add thumbnail generation to userimage api

--- src/Mcce.SmartOffice.UserImages/Migrations/20231202213214_AddThumbnailImageKey.Designer.cs
+++ src/Mcce.SmartOffice.UserImages/Migrations/20231202213214_AddThumbnailImageKey.Designer.cs
@@ -3,6 +3,7 @@ using Mcce.SmartOffice.UserImages;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Mcce.SmartOffice.UserImages.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231202213214_AddThumbnailImageKey")]
+    partial class AddThumbnailImageKey
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
